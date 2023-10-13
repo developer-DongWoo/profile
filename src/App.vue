605e5122d4eff758ef105576520feff4f3ba255f
developer-DongWoo <ehddn8547@gmail.com>
2023-10-13 18:38:36 +0900

    feat/fe/setting create vue files (#2)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="app">
-  </div>
+    <div id="app">
+        <Navbar/>
+        <router-view/>
+    </div>
 </template>
 
 <script>
-
+import Navbar from './components/Navbar.vue'
 export default {
-  name: 'App',
-  components: {
-  }
+    name: 'App',
+    components: {
+        Navbar
+    }
 }
 </script>
 
