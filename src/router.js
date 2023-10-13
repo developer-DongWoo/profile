e1bbb3c3fc617d8f45273ef31a06e37e644fe556
developer-DongWoo <ehddn8547@gmail.com>
2023-10-13 18:23:16 +0900

    feat/fe/setting router,vuex (#1)

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+
+Vue.use(VueRouter);
+
+const routes = [
+
+];
+
+const router = new VueRouter({
+    mode: 'history', // 히스토리 모드 활성화
+    routes
+  });
+  
+export default router;
---
+++ src/store.js
@@ -0,0 +1,18 @@
+// store.js
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        serverURL:"https://introduction.store"
+    },
+    mutations: {
+
+    },
+    actions: {
+    }
+    }
+);
